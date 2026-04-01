0eb2c04d0fe16e3b63502d2aac9204f27a3d3e19
matteolpera <matteo.lepera02@gmail.com>
2026-04-01 12:48:04 +0200

    feat: aggiunto il model Project.
    aggiunta migrations per tabella Projects.
    
    aggiunto seeder per popolare la tabella Projects.

---
+++ database/seeders/ProjectTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+
+class ProjectTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $newProject = new Project();
+
+            $newProject->name = $faker->catchPhrase();
+            $newProject->client = $faker->name();
+            $newProject->start_date = $faker->date('Y-m-d');
+            $newProject->end_date = $faker->date('Y-m-d');
+            $newProject->summary = $faker->paragraph(2);
+
+            $newProject->save();
+        }
+    }
+}
